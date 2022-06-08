685d0ddd4e11c29cd360c6c0c53eb8bcf169dcd0
Lukasz Guminski <lguminski@gmail.com>
2022-06-08 19:23:29 +0200

    test

--- function1.ex
+++ function1.ex
@@ -6,9 +6,8 @@ end
 defmodule MyApplication do
   use CyberOS.DSL, version: 1.0
 
-  alias MyTypes.{SensorProtocol}
-
   actor Sensor, type: Actor.OCIContainer do
+    output(:api, type: SensorProtocol.define(version: 1.0))
 
     @impl true
     def initialize(_this) do
