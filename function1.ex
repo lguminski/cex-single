d8ece4b662c2ed2798d47d2bb975c6a27149b68f
Lukasz Guminski <lguminski@gmail.com>
2022-06-08 19:21:13 +0200

    test

--- function1.ex
+++ function1.ex
@@ -1,30 +1,29 @@
 defmodule MyTypes do
   use CyberOS.DSL, version: 1.0
-  type(MySQL, implements: VersionedType)
+  type(SensorProtocol, implements: VersionedType)
 end
 
 defmodule MyApplication do
   use CyberOS.DSL, version: 1.0
 
-  alias MyTypes.{MySQL}
+  alias MyTypes.{SensorProtocol}
 
-  actor DatabaseProvider, type: Actor.OCIContainer do
-    output(:mysql12, type: MySQL.define(version: 8.0))
+  actor Sensor, type: Actor.OCIContainer do
 
     @impl true
     def initialize(_this) do
     end
   end
 
-  main do
+  composite ACA do
     param(:count, default: 18)
 
     @impl true
     def initialize(this) do
       for i <- 0..10,
           do:
-            {:ok, _database_provider} =
-              add_component(this, "database#{i}", DatabaseProvider, [], [])
+            {:ok, _sensor} =
+              add_component(this, "sensor #{i}", Sensor, [], [])
     end
   end
 end
